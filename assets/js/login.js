573932ceb13f08f2be5a142b9ac177518d7db0fc
Diego Meza <diego-oz@hotmail.com>
2023-08-14 20:58:23 -0600

    url

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,7 +5,7 @@ const mensajeErrorContrasenas = document.getElementById(
 );
 
 const loginPeticion = () => {
-  const url = "http://127.0.0.1:8000/api/autenticacion/login";
+  const url = enviroments.urlBase + "/api/autenticacion/login";
 
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
